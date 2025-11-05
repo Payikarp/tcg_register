0b29483772eeadc32a9b10788364e0cb41ec58e0
Payikarp <andresbecv@gmail.com>
2025-11-05 17:46:39 -0600

    Animation added on landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import "@/app/css/homeStyles.css"
 
 export default function Home() {
   return (
-    <>
-    <h1>React + </h1>
-    </>
+    <html>
+      <body>
+        <div id="container">
+            <div id="titleDiv">
+          <p className="titleClass">TCG Register</p>
+          <Image id="enterButton"
+          src="/images/enterButton.png"
+          width={100}
+          height={100}
+          alt="Enter">
+          </Image>
+          </div>
+          <div id="backgroundDiv">
+            <img src="https://sm.ign.com/t/ign_latam/screenshot/default/chainsaw-man-the-movie-reze-arc_r15w.1280.jpg"></img>
+          </div>
+        </div>
+    </body>
+    </html>
   );
 }
